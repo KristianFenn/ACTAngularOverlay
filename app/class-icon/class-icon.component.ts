346cacf133b1a44227c86e930b1852b8297e94d0
Kristian Fenn <kristianfenn1@gmail.com>
2017-01-22 18:35:10 +0000

    Fixed class icon size. Removed last views from app

--- app/class-icon/class-icon.component.ts
+++ app/class-icon/class-icon.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Player } from '../models/player.model';
+import { Configuration } from '../config';
 
 @Component({
     selector: 'class-icon',
-    templateUrl: 'app/class-icon/class-icon.html',
-    styleUrls: [ 'app/class-icon/class-icon.css' ]
+    templateUrl: Configuration.GetGlobalPath('class-icon.html'),
+    styleUrls: [ Configuration.GetGlobalPath('class-icon.css') ]
 })
 export class ClassIconComponent {
     @Input() playerClass: string;
+    @Input() iconSize: number;
 
     getIconSrc() {
         return "icons/" + this.playerClass + ".png";
--- app/config.ts
+++ app/config.ts
@@ -4,4 +4,8 @@ export class Configuration {
     static GetThemePath(fileName: string) {
         return 'themes/' + this.Theme + '/' + fileName;
     }
+
+    static GetGlobalPath(fileName: string) {
+        return 'themes/global/' + fileName;
+    }
 }
