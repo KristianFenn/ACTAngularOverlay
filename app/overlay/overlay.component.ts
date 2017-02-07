a269f80a5ec45bef3f95ad607cb4a32646f44762
Kristian Fenn <kristianfenn1@gmail.com>
2017-02-07 16:08:33 +0000

    Neatened up overlay class. Fixed margins on table theme.

--- app/overlay/overlay.component.ts
+++ app/overlay/overlay.component.ts
@@ -20,19 +20,22 @@ export default class OverlayComponent {
   updater: Updater;
   tableFields: Array<PlayerTableField>;
 
+  private mainPlayerFn = (p: Player) => p.isMainPlayer() ? 'main-player' : '';
+  private redTextFn = (v: number) => v > 0 ? 'text-red' : '';
+
   constructor(updater: Updater) {
     this.updater = updater;
     this.updater.subscribe((data) => this.encounter = data );
 
     this.tableFields = [
-      new PlayerTableField(10, (p) => p.rank,         "Rank"),
+      new PlayerTableField(10, (p) => p.rank,         "Rank", this.mainPlayerFn),
       new PlayerTableField(10, (p) => p.class,        "Class",  null, true),
-      new PlayerTableField(30, (p) => p.name,         "Player", (p) => p.isMainPlayer() ? "main-player" : ""),
-      new PlayerTableField(10, (p) => p.dps,          "DPS",    (p) => p.isMainPlayer() ? "main-player" : ""),
+      new PlayerTableField(30, (p) => p.name,         "Player", this.mainPlayerFn),
+      new PlayerTableField(10, (p) => p.dps,          "DPS",   this.mainPlayerFn),
       new PlayerTableField(40, (p) => p.maxhit,       "Highest Hit"),
       new PlayerTableField(10, (p) => p.critPercent,  "Crit%"),
-      new PlayerTableField(10, (p) => p.misses,       "Miss",   (p) => p.misses > 0 ? "text-red" : "" ),
-      new PlayerTableField(10, (p) => p.deaths,       "Death",  (p) => p.deaths > 0 ? "text-red" : "" )
+      new PlayerTableField(10, (p) => p.misses,       "Miss",   (p) => this.redTextFn(p.misses) ),
+      new PlayerTableField(10, (p) => p.deaths,       "Death",  (p) => this.redTextFn(p.deaths) )
     ];
   }
 
