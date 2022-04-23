71ce23313b95a2f62f5f06c51ef98feeea4b35b3
Kristian Fenn <kristianfenn1@gmail.com>
2022-04-23 19:10:02 +0100

    Publish only if build and tests succeed

--- publish.ps1
+++ publish.ps1
@@ -1,14 +1,32 @@
+param(
+    [switch]$WhatIf
+)
+
 Write-Host "Compiling";
-ng build;
+$buildOutput = & ng build 2>&1;
+
+$errors = $buildOutput -match "Error";
+if ($errors.Length -gt 0) {
+    Write-Host "Build failed.";
+    $errors;
+    exit;
+}
+
+Write-Host "Running Tests";
+$testOutput = & ng test 2>&1;
+Write-Host $testOutput[-1];
+if ($testOutput[-1] -match "FAILED") {
+    exit;
+}
 
 $dest = "\\SERVER\Sites\McParser";
 
 Write-Host "Deleting old files";
-Get-ChildItem $dest | Remove-Item -Recurse -Force
+Get-ChildItem $dest | Remove-Item -Recurse -Force -WhatIf:$WhatIf
 
 foreach ($target in Get-ChildItem ".\dist") {
     Write-Host "Copying ${target}";
-    Copy-Item $target.FullName -Destination $dest -Recurse
+    Copy-Item $target.FullName -Destination $dest -Recurse -WhatIf:$WhatIf
 }
 
 Write-Host "Published!";
